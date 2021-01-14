fc89ff5f2bf611f2becd4d1ef00250b10ad1ab94
Cho Seong week <tjddnr0217@gmail.com>
2021-01-14 21:15:19 +0900

    fix - 불필요 함수 제거

--- frontend/src/component/Post/PostSide.js
+++ frontend/src/component/Post/PostSide.js
@@ -29,22 +29,6 @@ function NaverMapAPI(props) {
   }
 }
 
-function getLocation() {
-  if (navigator.geolocation) { // GPS를 지원하면
-    navigator.geolocation.getCurrentPosition(function(position) {
-      //alert(position.coords.latitude + ' ' + position.coords.longitude);
-    }, function(error) {
-      console.error(error);
-    }, {
-      enableHighAccuracy: false,
-      maximumAge: 0,
-      timeout: Infinity
-    });
-  } else {
-    alert('GPS를 지원하지 않습니다');
-  }
-}
-
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
